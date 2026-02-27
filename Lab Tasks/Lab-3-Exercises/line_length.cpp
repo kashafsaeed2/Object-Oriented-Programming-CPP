08e8a123896887cae793b52160b5bf1f3e1d97d6
kashafsaeed2 <kashafsaeed274@gmail.com>
2026-02-27 09:29:11 -0800

    Update line_length.cpp
    2025-AI-319

--- Lab Tasks/Lab-3-Exercises/line_length.cpp
+++ Lab Tasks/Lab-3-Exercises/line_length.cpp
@@ -14,8 +14,8 @@ int main() {
     cin>>temp>>sp.x>>temp>>sp.y>>temp;
     cout<<"Enter the end point coordinates: "<<endl; // (0,0)
     cin>>temp>>ep.x>>temp>>ep.y>>temp;
-    t1 = pow(sp.x - ep.x, 2);
-    t2 = pow(sp.y - ep.y, 2);
+     t1 = (sp.x - ep.x) * (sp.x - ep.x);
+    t2 = (sp.y - ep.y) * (sp.y - ep.y);
     length = sqrt(t1+t2);
     cout<<"Length is: "<<length<<endl;
     return 0;
